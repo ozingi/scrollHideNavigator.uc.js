47687b91c7a1f28bbfa262fb920e078a97d8627d
green hand <ozingi@163.com>
2022-08-24 07:16:51 +0800

    Update scrollHideNavigator.uc.js
    优化动画

--- scrollHideNavigator.uc.js
+++ scrollHideNavigator.uc.js
@@ -8,35 +8,25 @@
 var navBar = document.getElementById("nav-bar");
 var titleBar = document.getElementById("titlebar");
 var appcontent1 = document.getElementById("appcontent");
-
-
-
+navBar.style.transition = "margin-top ease 0.3s, opacity ease 0.5s";
 appcontent1.onmouseover = function () {
-
     var scrollFunc = function (e) {
         e = e
         ////Firefox滑轮事件  
         if (e.detail > 0) { //当滑轮向上滚动时  
-            navBar.style.marginTop = "-"+navBar.clientHeight+1+"px";
+            titleBar.style.marginTop = "-" + titleBar.clientHeight + "px";
+            navBar.style.marginTop = "-" + navBar.clientHeight + 1 + "px";
             navBar.style.opacity = 0;
-
-            titleBar.style.marginTop = "-"+titleBar.clientHeight+"px";
-            titleBar.style.opacity = 0;
-
         }
-        if (e.detail < 0) { //当滑轮向下滚动时  
-            //console.log("滑轮向下滚动");
-            navBar.style.marginTop = "0px";
+        if (e.detail < 0) { //当滑轮向下滚动时
+            navBar.style.marginTop = titleBar.style.marginTop = "0px";
             navBar.style.opacity = 100;
-
-            titleBar.style.marginTop = "0px";
-            titleBar.style.opacity = 100;
-
         }
     }
     ////给页面可视部分绑定滑轮滚动事件  
     ////firefox  
     appcontent.addEventListener('DOMMouseScroll', scrollFunc, false);
 }
+
 
 
